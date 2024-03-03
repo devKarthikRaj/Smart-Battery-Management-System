97cb25b639b48149bf1d93da6d92012bde3380ee
devKarthikRaj <rocketraj2000@gmail.com>
2024-03-04 00:21:23 +0800

    This code can publish to thingspeak with errors

--- ESP32 Firmware/3s_BMS_ESP32_Fw_v1.0/3s_BMS_ESP32_Fw_v1.0.ino
+++ ESP32 Firmware/3s_BMS_ESP32_Fw_v1.0/3s_BMS_ESP32_Fw_v1.0.ino
@@ -5,75 +5,130 @@
     2. Convert battery voltage ADC value to actual voltage value
     3. Post actual voltage to ThingSpeak
     4. TBC...
+
+  WARNING: Remove sensitive info before pushing to Github
  ******************************************************************/
 
 #include <WiFi.h>
 #include "ThingSpeak.h"
 
-//ESP32 WiFi Vars
-const char* ssid = "WiFi Name";           // ***Remove before pushing*** 
-const char* password = "WiFi Password";   //***Remove before pushing***
-WiFiClient  client;
+//Pin Definitions
+#define cell1Pin 32  //Cell 1 voltage pin
+#define cell2Pin 33  //Cell 2 voltage pin
+#define cell3Pin 35  //Cell 3 voltage pin
 
-//ThingSpeak Vars
-unsigned long myChannelId = Channel ID;     //***Remove before pushing***
-const char *myWriteAPIKey = "Write API Key";//***Remove before pushing***
-const char *myReadAPIKey = "Read API Key";  //***Remove before pushing***
+//WiFi Vars
+const char* ssid = "ssid";         // ***Remove before pushing***
+const char* pwd = "pwd";  //***Remove before pushing***
+WiFiClient client;
 
-// Data Var
-float randomData;
+//Cell Monitoring Vars
+float cell1Voltage;
+float cell2Voltage;
+float cell3Voltage;
 
 void setup() {
-  Serial.begin(115200);  //Initialize serial
-  
-  WiFi.mode(WIFI_STA);   //Set WiFi mode to station mode - ESP32 connects to an access point
-  
+  Serial.begin(115200);  //Init serial
+
+  ConnectWifi(); //This is a blocking function!!!
+
   ThingSpeak.begin(client);  //Initialize ThingSpeak
+
+  pinMode(cell1Pin, INPUT);
+  pinMode(cell2Pin, INPUT);
+  pinMode(cell3Pin, INPUT);
 }
 
 void loop() {
-    // Connect/reconnect to WiFi
-    if(WiFi.status() != WL_CONNECTED){
-      Serial.print("Attempting to connect");
-      Serial.print(".");
-      while(WiFi.status() != WL_CONNECTED){
-        WiFi.begin(ssid, password); 
-        Serial.print(".");
-        delay(5000); //Removing this delay will cause the ESP32 to crash while connecting to a wifi network
-      } 
-      Serial.println("\nConnected.");
-    }
+  ConnectWifi(); //ConnectWifi called again in case Wifi abruptly disconnects 
 
-    //Enter a random value into randomData
-    randomData = 36.6;
+  //Read cell voltages
+  cell1Voltage = ReadCellVoltage(cell1Pin);
+  cell2Voltage = 3.93; //Testing
+  cell2Voltage = 3.56; //Testing
+  //cell2Voltage = ReadCellVoltage(cell2Pin);
+  //cell3Voltage = ReadCellVoltage(cell3Pin);
+
+  //Publish cell voltages to ThingSpeak
+  Serial.println(cell1Voltage);
+  bool cell1UploadStatus = ThingSpeakWriteFloat(cell1Voltage,1);
+  bool cell2UploadStatus = ThingSpeakWriteFloat(cell2Voltage,2);
+  bool cell3UploadStatus = ThingSpeakWriteFloat(cell3Voltage,3);
+
+  //Debugging
+  if(!cell1UploadStatus) {
+    Serial.println("Failed to Publish Cell 1 Voltage to ThingSpeak");
+  }
+  if(!cell2UploadStatus) {
+    Serial.println("\nFailed to Publish Cell 2 Voltage to ThingSpeak");
+  }
+  if(!cell3UploadStatus) {
+    Serial.println("\nFailed to Publish Cell 3 Voltage to ThingSpeak");
+  }
     
-    //--------------------------------------------Write--------------------------------------------------
-    /*Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
-      pieces of information in a channel.  Here, we write to field 1 */
-    int writeStatusCode = ThingSpeak.writeField(myChannelId, 1, randomData, myWriteAPIKey);
+  if(cell1UploadStatus && cell2UploadStatus && cell3UploadStatus) {
+    Serial.println("\nSuccessfully Published Cell Voltages to ThingSpeak");
+  }
+}
 
-    //ThingSpeak will return HTTP status code 200 if successful
-    if(writeStatusCode == 200){
-      Serial.println("Channel update successful.");
-    } else {
-      Serial.println("Problem updating channel. HTTP error code " + String(writeStatusCode));
+void ConnectWifi() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Connecting to Wifi");
+    while (WiFi.status() != WL_CONNECTED) {
+      WiFi.begin(ssid, pwd);
+      Serial.print(".");
+      delay(5000);  //Removing this delay will cause the ESP32 to crash while connecting to a wifi network
     }
+    Serial.println("\nConnected to Wifi");
+  }
+}
 
-    //If the data packets are sent out in under 15s intervals, HTTP status code 401 will be returned
-    //Some packets will still be sent but this is not recommended
-    //So...Min delay is 15s
-    delay(20000);
+float ReadCellVoltage(int cellVoltageSensePin) {
+  //Conversion Vars
+  const float conversionFactor = 4.2 / 4096;  //Li ion max cell voltage = 4.2 and ESP32 ADC = 12 bits so 2^12 = 4096
+  const float conversionOffset = -0.1;        //ADC conversion offset
 
-    //---------------------------------------------Read--------------------------------------------------
-    //Read data from ThingSpeak
-    float readData = ThingSpeak.readFloatField(myChannelId, 1, myReadAPIKey);
-    int readStatusCode = ThingSpeak.getLastReadStatus();
-    //ThingSpeak will return HTTP status code 200 if successful
-    if(readStatusCode == 200){
-      Serial.println(String(readData));
-    } else {
-      Serial.println("Problem reading from channel. HTTP error code " + String(readStatusCode));
-    }
+  int cellVoltageRaw = analogRead(cellVoltageSensePin);
+  float cellVoltageActual = (cellVoltageRaw * conversionFactor) + conversionOffset;
+  
+  //Debugging
+  Serial.println();
+  Serial.print("Cell Voltage Raw: ");
+  Serial.print(cellVoltageRaw);
+  Serial.print(" Cell Voltage Actual: ");
+  Serial.print(cellVoltageActual);
+  Serial.println();
 
-    delay(20000);
+  return cellVoltageActual;
+}
+
+bool ThingSpeakWriteFloat(float data, int field) {
+  //ThingSpeak Vars
+  unsigned long myChannelId = Channel_ID;             //***Remove before pushing***
+  const char* myWriteAPIKey = "";  //***Remove before pushing***
+  const char* myReadAPIKey = "";   //***Remove before pushing***
+  int writeStatus = false;
+
+  //ThingSpeak.writeField(channel_id, field_num, data, write_api_key)
+  int writeStatusCode = ThingSpeak.writeField(myChannelId, field, data, myWriteAPIKey);
+
+  //ThingSpeak will return HTTP status code 200 if successful
+  if (writeStatusCode == 200) {
+    //Serial.println("Channel update successful.");
+    writeStatus = true;
+  } else {
+    Serial.println("Problem updating ThingSpeak channel. HTTP error code " + String(writeStatusCode));
+    writeStatus = false;
+  }
+
+  //If the data packets are sent out in under 15s intervals, HTTP status code 401 will be returned
+  //Some packets will still be sent but this is not recommended
+  //So set min delay to atleast 15s
+  delay(20000);
+
+  if(writeStatus) {
+    return true;    
+  } else {
+    return false;
+  }
 }
